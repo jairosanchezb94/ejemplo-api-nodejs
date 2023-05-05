f3ff64dcb4b157f7eb680517c26e934bc0be5fd5
Jairo Sánchez <jairosanchezb5@gmail.com>
2023-05-05 10:51:34 +0200

    mejoras

--- src/v1/swagger.js
+++ src/v1/swagger.js
@@ -5,7 +5,7 @@ const swaggerUi = require("swagger-ui-express");
 const options = {
   definition: {
     openapi: "3.0.0",
-    info: { title: "Crossfit WOD API", version: "1.0.0" },
+    info: { title: "Ejemplo Api NodeJs", version: "1.0.0" },
   },
   apis: ["src/v1/routes/workoutRoutes.js", "src/database/Workout.js"],
 };
